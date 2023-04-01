c89bfa632a42bcc44798964247b80f6b94e77da6
thien0709 <thien070904@gmail.com>
2023-04-01 18:46:37 +0700

    update bar

--- access/main.js
+++ access/main.js
@@ -1,8 +1,18 @@
+// Click button 
+const bar = document.querySelector("#tool .bar .icon1");
+const more = document.querySelector("#tool .more");
+const tool =document.querySelector("#tool");
+bar.addEventListener("click", function (){
+    tool.classList.toggle("active");
+    more.classList.toggle("active");
+})
+
 const icon = document.querySelector("#notion");
 const notion = document.querySelector("#notion h6");
 const button = document.querySelectorAll("#tool .more i");
-for(let i=0;i<button.length;i++){
-    button[i].addEventListener("mouseenter", function(){
-        icon.style.transform = 'translateY(2rem)';
-    })
-}
+
+// for(let i=0;i<button.length;i++){
+//     button[i].addEventListener("mouseenter", function(){
+//         icon.style.transform = 'translateY(2rem)';
+//     })
+// }
