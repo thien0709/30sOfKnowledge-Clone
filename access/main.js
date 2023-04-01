d75e36cff132be59f94a4b33c7cd3b648555e065
thien0709 <thien070904@gmail.com>
2023-04-01 11:21:44 +0700

    last commit

--- access/main.js
+++ access/main.js
@@ -1,2 +1,8 @@
+const icon = document.querySelector("#notion");
 const notion = document.querySelector("#notion h6");
-console.log(notion)
+const button = document.querySelectorAll("#tool .more i");
+for(let i=0;i<button.length;i++){
+    button[i].addEventListener("mouseenter", function(){
+        icon.style.transform = 'translateY(2rem)';
+    })
+}
